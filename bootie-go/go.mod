a57295876275d2b8ed971b8977dd6474d59213a4
BachNX <thanbaiks@gmail.com>
2025-04-14 22:53:28 +0700

    Better go disk scan

--- bootie-go/go.mod
+++ bootie-go/go.mod
@@ -6,3 +6,5 @@ require (
 	golang.org/x/sys v0.32.0
 	howett.net/plist v1.0.1
 )
+
+require github.com/gocarina/gocsv v0.0.0-20240520201108-78e41c74b4b1 // indirect
--- bootie-go/scandisk_windows.go
+++ bootie-go/scandisk_windows.go
@@ -2,11 +2,41 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
 
-func getDiskEntryByIdentifier(target string) (diskEntry, error) {
+	"github.com/gocarina/gocsv"
+)
+
+type csvEntry struct {
+	Caption    string
+	Node       string
+	DeviceID   string
+	Model      string
+	Partitions int32
+	Size       int64
 }
 
 func scanDisk() ([]diskEntry, error) {
+	csvEntries := []csvEntry{}
+	result := []diskEntry{}
+	cmd := exec.Command("wmic", "diskdrive", "list", "/format:csv")
+	raw, err := cmd.Output()
+
+	if err != nil {
+		return nil, err
+	}
+
+	gocsv.UnmarshalBytes(raw, &csvEntries)
+
+	for _, entry := range csvEntries {
+		result = append(result, diskEntry{
+			identifier: entry.DeviceID,
+			label:      entry.Caption,
+			size:       entry.Size,
+		})
+	}
+
 	return nil, fmt.Errorf("not implemented")
 }
